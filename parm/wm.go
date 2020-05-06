24881669f30b9ba4260ef5a2e68f2a07b8758ac0
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-06 19:21:04 -0300

    WM button messages.

--- parm/wm.go
+++ parm/wm.go
@@ -8,44 +8,68 @@ import (
 
 type WmActivate Raw
 
-func Event(p *WmActivate) c.WA              { return c.WA(api.LoWord(uint32(p.LParam))) }
-func IsMinimized(p *WmActivate) bool        { return api.HiWord(uint32(p.LParam)) != 0 }
-func PreviousWindow(p *WmActivate) api.HWND { return api.HWND(p.LParam) }
+func Event(p WmActivate) c.WA              { return c.WA(api.LoWord(uint32(p.LParam))) }
+func IsMinimized(p WmActivate) bool        { return api.HiWord(uint32(p.LParam)) != 0 }
+func PreviousWindow(p WmActivate) api.HWND { return api.HWND(p.LParam) }
 
 type WmClose Raw
 
 type WmCommand Raw
 
-func (p *WmCommand) IsFromMenu() bool         { return api.HiWord(uint32(p.WParam)) == 0 }
-func (p *WmCommand) IsFromAccelerator() bool  { return api.HiWord(uint32(p.WParam)) == 1 }
-func (p *WmCommand) IsFromControl() bool      { return !p.IsFromMenu() && !p.IsFromAccelerator() }
-func (p *WmCommand) MenuId() c.ID             { return p.ControlId() }
-func (p *WmCommand) AcceleratorId() c.ID      { return p.ControlId() }
-func (p *WmCommand) ControlId() c.ID          { return c.ID(api.LoWord(uint32(p.WParam))) }
-func (p *WmCommand) ControlNotifCode() uint16 { return api.HiWord(uint32(p.WParam)) }
-func (p *WmCommand) ControlHwnd() api.HWND    { return api.HWND(p.LParam) }
+func (p WmCommand) IsFromMenu() bool         { return api.HiWord(uint32(p.WParam)) == 0 }
+func (p WmCommand) IsFromAccelerator() bool  { return api.HiWord(uint32(p.WParam)) == 1 }
+func (p WmCommand) IsFromControl() bool      { return !p.IsFromMenu() && !p.IsFromAccelerator() }
+func (p WmCommand) MenuId() c.ID             { return p.ControlId() }
+func (p WmCommand) AcceleratorId() c.ID      { return p.ControlId() }
+func (p WmCommand) ControlId() c.ID          { return c.ID(api.LoWord(uint32(p.WParam))) }
+func (p WmCommand) ControlNotifCode() uint16 { return api.HiWord(uint32(p.WParam)) }
+func (p WmCommand) ControlHwnd() api.HWND    { return api.HWND(p.LParam) }
 
 type WmCreate Raw
 
-func (p *WmCreate) CreateStruct() *api.CREATESTRUCT {
+func (p WmCreate) CreateStruct() *api.CREATESTRUCT {
 	return (*api.CREATESTRUCT)(unsafe.Pointer(p.LParam))
 }
 
 type WmDestroy Raw
 
+type WmLButtonDblClk Raw // inherit
+
+func (p WmLButtonDblClk) HasCtrl() bool      { return (c.MK(p.WParam) & c.MK_CONTROL) != 0 }
+func (p WmLButtonDblClk) HasLeftBtn() bool   { return (c.MK(p.WParam) & c.MK_LBUTTON) != 0 }
+func (p WmLButtonDblClk) HasMiddleBtn() bool { return (c.MK(p.WParam) & c.MK_MBUTTON) != 0 }
+func (p WmLButtonDblClk) HasRightBtn() bool  { return (c.MK(p.WParam) & c.MK_RBUTTON) != 0 }
+func (p WmLButtonDblClk) HasShift() bool     { return (c.MK(p.WParam) & c.MK_SHIFT) != 0 }
+func (p WmLButtonDblClk) HasXBtn1() bool     { return (c.MK(p.WParam) & c.MK_XBUTTON1) != 0 }
+func (p WmLButtonDblClk) HasXBtn2() bool     { return (c.MK(p.WParam) & c.MK_XBUTTON2) != 0 }
+func (p WmLButtonDblClk) Pos() *api.POINT {
+	return &api.POINT{int32(api.LoWord(uint32(p.LParam))), int32(api.HiWord(uint32(p.LParam)))}
+}
+
+type WmLButtonDown struct{ WmLButtonDblClk } // inherit
+type WmLButtonUp struct{ WmLButtonDblClk }
+type WmMButtonDblClk struct{ WmLButtonDblClk }
+type WmMButtonDown struct{ WmLButtonDblClk }
+type WmMButtonUp struct{ WmLButtonDblClk }
+type WmMouseHover struct{ WmLButtonDblClk }
+type WmMouseMove struct{ WmLButtonDblClk }
+type WmRButtonDblClk struct{ WmLButtonDblClk }
+type WmRButtonDown struct{ WmLButtonDblClk }
+type WmRButtonUp struct{ WmLButtonDblClk }
+
 type WmNcDestroy Raw
 
 type WmNcPaint Raw
 
-func (p *WmNcPaint) Hrgn() api.HRGN { return api.HRGN(p.LParam) }
+func (p WmNcPaint) Hrgn() api.HRGN { return api.HRGN(p.LParam) }
 
 type WmNotify Raw
 
-func (p *WmNotify) NmHdr() *api.NMHDR { return (*api.NMHDR)(unsafe.Pointer(p.LParam)) }
+func (p WmNotify) NmHdr() *api.NMHDR { return (*api.NMHDR)(unsafe.Pointer(p.LParam)) }
 
 type WmSize Raw
 
-func Request(p *WmSize) c.SIZE { return c.SIZE(p.WParam) }
-func Size(p *WmSize) api.SIZE {
+func Request(p WmSize) c.SIZE { return c.SIZE(p.WParam) }
+func Size(p WmSize) api.SIZE {
 	return api.SIZE{Cx: int32(api.LoWord(uint32(p.LParam))), Cy: int32(api.HiWord(uint32(p.LParam)))}
 }
--- ui/windowOnWm.go
+++ ui/windowOnWm.go
@@ -36,6 +36,77 @@ func (me *windowOn) WmDestroy(userFunc func(p parm.WmDestroy)) {
 	}
 }
 
+func (me *windowOn) WmLButtonDblClk(userFunc func(p parm.WmLButtonDblClk)) {
+	me.msgs[c.WM_LBUTTONDBLCLK] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmLButtonDblClk(p))
+		return 0
+	}
+}
+func (me *windowOn) WmLButtonDown(userFunc func(p parm.WmLButtonDown)) {
+	me.msgs[c.WM_LBUTTONDOWN] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmLButtonDown{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+func (me *windowOn) WmLButtonUp(userFunc func(p parm.WmLButtonUp)) {
+	me.msgs[c.WM_LBUTTONUP] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmLButtonUp{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+
+func (me *windowOn) WmMButtonDblClk(userFunc func(p parm.WmMButtonDblClk)) {
+	me.msgs[c.WM_MBUTTONDBLCLK] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmMButtonDblClk{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+func (me *windowOn) WmMButtonDown(userFunc func(p parm.WmMButtonDown)) {
+	me.msgs[c.WM_MBUTTONDOWN] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmMButtonDown{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+func (me *windowOn) WmMButtonUp(userFunc func(p parm.WmMButtonUp)) {
+	me.msgs[c.WM_MBUTTONUP] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmMButtonUp{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+
+func (me *windowOn) WmMouseHover(userFunc func(p parm.WmMouseHover)) {
+	me.msgs[c.WM_MOUSEHOVER] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmMouseHover{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+
+func (me *windowOn) WmMouseMove(userFunc func(p parm.WmMouseMove)) {
+	me.msgs[c.WM_MOUSEMOVE] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmMouseMove{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+
+func (me *windowOn) WmRButtonDblClk(userFunc func(p parm.WmRButtonDblClk)) {
+	me.msgs[c.WM_RBUTTONDBLCLK] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmRButtonDblClk{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+func (me *windowOn) WmRButtonDown(userFunc func(p parm.WmRButtonDown)) {
+	me.msgs[c.WM_RBUTTONDOWN] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmRButtonDown{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+func (me *windowOn) WmRButtonUp(userFunc func(p parm.WmRButtonUp)) {
+	me.msgs[c.WM_RBUTTONUP] = func(p parm.Raw) uintptr {
+		userFunc(parm.WmRButtonUp{WmLButtonDblClk: parm.WmLButtonDblClk(p)})
+		return 0
+	}
+}
+
 func (me *windowOn) WmNcDestroy(userFunc func(p parm.WmNcDestroy)) {
 	me.msgs[c.WM_NCDESTROY] = func(p parm.Raw) uintptr {
 		userFunc(parm.WmNcDestroy(p))
