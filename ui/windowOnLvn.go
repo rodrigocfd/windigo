556ae871f630073f5b5fd6eaa4e3b686cb33c904
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-07 23:23:38 -0300

    Simplifying WM_NOTIFY handlers.

--- ui/windowOnLvn.go
+++ ui/windowOnLvn.go
@@ -1,47 +1,50 @@
 package ui
 
 import (
+	"gowinui/api"
 	c "gowinui/consts"
 	"gowinui/parm"
+	"unsafe"
 )
 
-func (me *windowOn) LvnDeleteAllItems(lv *ListView, userFunc func(p parm.LvnDeleteAllItems)) {
+func (me *windowOn) LvnDeleteAllItems(lv *ListView, userFunc func(p *api.NMLISTVIEW)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_DELETEALLITEMS), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnDeleteAllItems(p))
+		userFunc((*api.NMLISTVIEW)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
-func (me *windowOn) LvnDeleteItem(lv *ListView, userFunc func(p parm.LvnDeleteItem)) {
+
+func (me *windowOn) LvnDeleteItem(lv *ListView, userFunc func(p *api.NMLISTVIEW)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_DELETEITEM), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnDeleteItem{LvnDeleteAllItems: parm.LvnDeleteAllItems(p)})
+		userFunc((*api.NMLISTVIEW)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
-func (me *windowOn) LvnInsertItem(lv *ListView, userFunc func(p parm.LvnInsertItem)) {
+
+func (me *windowOn) LvnInsertItem(lv *ListView, userFunc func(p *api.NMLISTVIEW)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_INSERTITEM), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnInsertItem{LvnDeleteAllItems: parm.LvnDeleteAllItems(p)})
+		userFunc((*api.NMLISTVIEW)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
-func (me *windowOn) LvnItemChanged(lv *ListView, userFunc func(p parm.LvnItemChanged)) {
+
+func (me *windowOn) LvnItemChanged(lv *ListView, userFunc func(p *api.NMLISTVIEW)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_ITEMCHANGED), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnItemChanged{LvnDeleteAllItems: parm.LvnDeleteAllItems(p)})
+		userFunc((*api.NMLISTVIEW)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
 
-//------------------------------------------------------------------------------
-
-func (me *windowOn) LvnItemActivate(lv *ListView, userFunc func(p parm.LvnItemActivate)) {
+func (me *windowOn) LvnItemActivate(lv *ListView, userFunc func(p *api.NMITEMACTIVATE)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_ITEMACTIVATE), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnItemActivate(p))
+		userFunc((*api.NMITEMACTIVATE)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
 
-func (me *windowOn) LvnKeyDown(lv *ListView, userFunc func(p parm.LvnKeyDown)) {
+func (me *windowOn) LvnKeyDown(lv *ListView, userFunc func(p *api.NMLVKEYDOWN)) {
 	me.addNfy(lv.CtrlId(), int32(c.LVN_KEYDOWN), func(p parm.WmNotify) uintptr {
-		userFunc(parm.LvnKeyDown(p))
+		userFunc((*api.NMLVKEYDOWN)(unsafe.Pointer(p.LParam)))
 		return 0
 	})
 }
