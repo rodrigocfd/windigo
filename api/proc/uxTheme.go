95c1b5ec6152370b178300afe030dc86de8a0e86
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-11 12:36:06 -0300

    MIT license, 55 files.

--- api/proc/uxTheme.go
+++ api/proc/uxTheme.go
@@ -1,3 +1,10 @@
+/**
+ * Part of Wingows - Win32 API layer for Go
+ * https://github.com/rodrigocfd/wingows
+ * Copyright 2020-present Rodrigo Cesar de Freitas Dias
+ * This library is released under the MIT license
+ */
+
 package proc
 
 import (
