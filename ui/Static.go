268b6023f55abba597f3ae6c8fca1247b4f38bcf
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-20 11:35:16 -0300

    Counting label mnemonic ampersands for size.

--- ui/Static.go
+++ ui/Static.go
@@ -7,6 +7,7 @@
 package ui
 
 import (
+	"strings"
 	c "wingows/consts"
 )
 
@@ -33,19 +34,41 @@ func (me *Static) Create(parent Window, x, y int32, width, height uint32,
 	return me
 }
 
-func (me *Static) CreateSimple(parent Window, x, y int32, text string) *Static {
-	cx, cy := me.calcIdealSize(parent, text)
+func (me *Static) CreateLText(parent Window, x, y int32, text string) *Static {
+	cx, cy := me.calcIdealSize(parent, text, true)
 	return me.Create(parent, x, y, cx, cy, text,
-		c.WS_EX(0), c.WS_CHILD|c.WS_VISIBLE, c.SS_LEFT|c.SS_WORDELLIPSIS)
+		c.WS_EX(0), c.WS_CHILD|c.WS_GROUP|c.WS_VISIBLE, c.SS_LEFT)
 }
 
-func (me *Static) calcIdealSize(parent Window, text string) (uint32, uint32) {
+func (me *Static) calcIdealSize(parent Window, text string,
+	ampersandIsUnderline bool) (uint32, uint32) {
+
 	parentDc := parent.Hwnd().GetDC()
 	cloneDc := parentDc.CreateCompatibleDC()
 	prevFont := cloneDc.SelectObjectFont(globalUiFont.Hfont())
+
+	if ampersandIsUnderline {
+		text = me.removeAmpersands(text)
+	}
+
 	bounds := cloneDc.GetTextExtentPoint32(text) // counting &, must remove!
 	cloneDc.SelectObjectFont(prevFont)
 	cloneDc.DeleteDC()
 	parent.Hwnd().ReleaseDC(parentDc)
+
 	return uint32(bounds.Cx), uint32(bounds.Cy)
+}
+
+func (me *Static) removeAmpersands(text string) string {
+	buf := strings.Builder{}
+	for i := 0; i < len(text)-1; i++ {
+		if text[i] == '&' && text[i+1] != '&' {
+			continue
+		}
+		buf.WriteByte(text[i])
+	}
+	if text[len(text)-1] != '&' {
+		buf.WriteByte(text[len(text)-1])
+	}
+	return buf.String()
 }
