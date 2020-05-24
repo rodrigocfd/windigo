ff4a3fd54d8cea7684a0c9ea47f4e531b1759f42
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-24 13:38:25 -0300

    All list view stuff in a single file.

--- ui/ListView.go
+++ ui/ListView.go
@@ -8,6 +8,7 @@ package ui
 
 import (
 	"fmt"
+	"syscall"
 	"unsafe"
 	"wingows/api"
 )
@@ -180,4 +181,221 @@ func (me *ListView) sendLvmMessage(msg api.LVM,
 
 	return me.controlNativeBase.Hwnd().
 		SendMessage(api.WM(msg), wParam, lParam) // simple wrapper
+}
+
+//------------------------------------------------------------------------------
+
+// A single column of a list view control.
+type ListViewColumn struct {
+	owner *ListView
+	index uint32
+}
+
+func newListViewColumn(owner *ListView, index uint32) *ListViewColumn {
+	return &ListViewColumn{
+		owner: owner,
+		index: index,
+	}
+}
+
+func (me *ListViewColumn) Index() uint32 {
+	return me.index
+}
+
+func (me *ListViewColumn) SetText(text string) *ListViewColumn {
+	lvc := api.LVCOLUMN{
+		ISubItem: int32(me.index),
+		Mask:     api.LVCF_TEXT,
+		PszText:  api.StrToUtf16Ptr(text),
+	}
+	ret := me.owner.sendLvmMessage(api.LVM_SETCOLUMN,
+		api.WPARAM(me.index), api.LPARAM(unsafe.Pointer(&lvc)))
+	if ret == 0 {
+		panic(fmt.Sprintf("LVM_SETCOLUMN failed to set text \"%s\".", text))
+	}
+	return me
+}
+
+func (me *ListViewColumn) SetWidth(width uint32) *ListViewColumn {
+	me.owner.sendLvmMessage(api.LVM_SETCOLUMNWIDTH,
+		api.WPARAM(me.index), api.LPARAM(width))
+	return me
+}
+
+func (me *ListViewColumn) Text() string {
+	buf := make([]uint16, 256) // arbitrary
+	lvc := api.LVCOLUMN{
+		ISubItem:   int32(me.index),
+		Mask:       api.LVCF_TEXT,
+		PszText:    &buf[0],
+		CchTextMax: int32(len(buf)),
+	}
+	ret := me.owner.sendLvmMessage(api.LVM_GETCOLUMN,
+		api.WPARAM(me.index), api.LPARAM(unsafe.Pointer(&lvc)))
+	if ret < 0 {
+		panic("LVM_GETCOLUMN failed to get text.")
+	}
+	return syscall.UTF16ToString(buf)
+}
+
+func (me *ListViewColumn) Width() uint32 {
+	cx := me.owner.sendLvmMessage(api.LVM_GETCOLUMNWIDTH, api.WPARAM(me.index), 0)
+	if cx == 0 {
+		panic("LVM_GETCOLUMNWIDTH failed.")
+	}
+	return uint32(cx)
+}
+
+//------------------------------------------------------------------------------
+
+// A single item row of a list view control.
+type ListViewItem struct {
+	owner *ListView
+	index uint32
+}
+
+func newListViewItem(owner *ListView, index uint32) *ListViewItem {
+	return &ListViewItem{
+		owner: owner,
+		index: index,
+	}
+}
+
+func (me *ListViewItem) Delete() {
+	if me.index >= me.owner.ItemCount() { // index out of bounds: ignore
+		return
+	}
+
+	ret := me.owner.sendLvmMessage(api.LVM_DELETEITEM,
+		api.WPARAM(me.index), 0)
+	if ret == 0 {
+		panic(fmt.Sprintf("LVM_DELETEITEM failed, index %d.\n", me.index))
+	}
+}
+
+func (me *ListViewItem) Index() uint32 {
+	return me.index
+}
+
+func (me *ListViewItem) IsCut() bool {
+	sta := me.owner.sendLvmMessage(api.LVM_GETITEMSTATE,
+		api.WPARAM(me.index), api.LPARAM(api.LVIS_CUT))
+	return (api.LVIS(sta) & api.LVIS_CUT) != 0
+}
+
+func (me *ListViewItem) IsFocused() bool {
+	sta := me.owner.sendLvmMessage(api.LVM_GETITEMSTATE,
+		api.WPARAM(me.index), api.LPARAM(api.LVIS_FOCUSED))
+	return (api.LVIS(sta) & api.LVIS_FOCUSED) != 0
+}
+
+func (me *ListViewItem) IsSelected() bool {
+	sta := me.owner.sendLvmMessage(api.LVM_GETITEMSTATE,
+		api.WPARAM(me.index), api.LPARAM(api.LVIS_SELECTED))
+	return (api.LVIS(sta) & api.LVIS_SELECTED) != 0
+}
+
+func (me *ListViewItem) IsVisible() bool {
+	return me.owner.sendLvmMessage(api.LVM_ISITEMVISIBLE,
+		api.WPARAM(me.index), 0) != 0
+}
+
+func (me *ListViewItem) SetFocus() *ListViewItem {
+	lvi := api.LVITEM{
+		StateMask: api.LVIS_FOCUSED,
+		State:     api.LVIS_FOCUSED,
+	}
+	ret := me.owner.sendLvmMessage(api.LVM_SETITEMSTATE,
+		api.WPARAM(me.index), api.LPARAM(unsafe.Pointer(&lvi)))
+	if ret == 0 {
+		panic("LVM_SETITEMSTATE failed for LVIS_FOCUSED.")
+	}
+	return me
+}
+
+func (me *ListViewItem) SetSelected(selected bool) *ListViewItem {
+	lvi := api.LVITEM{
+		StateMask: api.LVIS_SELECTED,
+	}
+	if selected { // otherwise remains zero
+		lvi.State = api.LVIS_SELECTED
+	}
+	ret := me.owner.sendLvmMessage(api.LVM_SETITEMSTATE,
+		api.WPARAM(me.index), api.LPARAM(unsafe.Pointer(&lvi)))
+	if ret == 0 {
+		panic("LVM_SETITEMSTATE failed for LVIS_SELECTED.")
+	}
+	return me
+}
+
+func (me *ListViewItem) SetText(text string) *ListViewItem {
+	me.SubItem(0).SetText(text)
+	return me
+}
+
+func (me *ListViewItem) SubItem(index uint32) *ListViewSubItem {
+	numCols := me.owner.ColumnCount()
+	if index >= numCols {
+		panic("Trying to retrieve sub item with index out of bounds.")
+	}
+	return newListViewSubItem(me, index)
+}
+
+func (me *ListViewItem) Text() string {
+	return me.SubItem(0).Text()
+}
+
+func (me *ListViewItem) Update() *ListViewItem {
+	ret := me.owner.sendLvmMessage(api.LVM_UPDATE, api.WPARAM(me.index), 0)
+	if ret == 0 {
+		panic("LVM_UPDATE failed.")
+	}
+	return me
+}
+
+//------------------------------------------------------------------------------
+
+// A cell from a list view item row.
+type ListViewSubItem struct {
+	item  *ListViewItem
+	index uint32
+}
+
+func newListViewSubItem(item *ListViewItem, index uint32) *ListViewSubItem {
+	return &ListViewSubItem{
+		item:  item,
+		index: index,
+	}
+}
+
+func (me *ListViewSubItem) Index() uint32 {
+	return me.index
+}
+
+func (me *ListViewSubItem) SetText(text string) *ListViewSubItem {
+	lvi := api.LVITEM{
+		ISubItem: int32(me.index),
+		PszText:  api.StrToUtf16Ptr(text),
+	}
+	ret := me.item.owner.sendLvmMessage(api.LVM_SETITEMTEXT,
+		api.WPARAM(me.item.index), api.LPARAM(unsafe.Pointer(&lvi)))
+	if ret == 0 {
+		panic(fmt.Sprintf("LVM_SETITEMTEXT failed \"%s\".", text))
+	}
+	return me
+}
+
+func (me *ListViewSubItem) Text() string {
+	buf := make([]uint16, 256) // arbitrary
+	lvi := api.LVITEM{
+		ISubItem:   int32(me.index),
+		PszText:    &buf[0],
+		CchTextMax: int32(len(buf)),
+	}
+	ret := me.item.owner.sendLvmMessage(api.LVM_GETITEMTEXT,
+		api.WPARAM(me.item.index), api.LPARAM(unsafe.Pointer(&lvi)))
+	if ret < 0 {
+		panic("LVM_GETITEMTEXT failed.")
+	}
+	return syscall.UTF16ToString(buf)
 }
