f56922c98df5bcae4ef17640028d5912c9f0c280
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-07-07 00:43:46 -0300

    Menu entries kept in a map.

--- gui/Menu.go
+++ gui/Menu.go
@@ -16,51 +16,48 @@ import (
 
 // Manages an HMENU resource.
 type Menu struct {
-	text     string // the root will have an empty string
 	hMenu    win.HMENU
-	children []interface{} // Menu, menuItem, menuSeparator
+	children map[string]interface{} // Menu or menuItem
 }
 
 // Inserts a new item with the given text.
 // The command ID will be automatically generated.
 func (me *Menu) AppendItem(text string) *Menu {
-	newItem := &menuItem{text: text}
+	newItem := &menuItem{}
 	me.hMenu.AppendMenu(co.MF_STRING, uintptr(newItem.Id()),
 		uintptr(unsafe.Pointer(win.StrToPtr(text))))
-	me.children = append(me.children, newItem)
+	me.children[removeAccelAmpersands(text)] = newItem // text kept without ampersands
 	return me
 }
 
 // Inserts a new item with the given text, and a specific command ID.
 func (me *Menu) AppendItemWithId(text string, cmdId int32) *Menu {
 	newItem := &menuItem{
-		text:  text,
 		cmdId: controlId{id: cmdId},
 	}
 	me.hMenu.AppendMenu(co.MF_STRING, uintptr(cmdId),
 		uintptr(unsafe.Pointer(win.StrToPtr(text))))
-	me.children = append(me.children, newItem)
+	me.children[removeAccelAmpersands(text)] = newItem // text kept without ampersands
 	return me
 }
 
 // Inserts a separator.
 func (me *Menu) AppendSeparator() *Menu {
-	me.hMenu.AppendMenu(co.MF_SEPARATOR, 0, 0)
-	me.children = append(me.children, &menuSeparator{})
+	me.hMenu.AppendMenu(co.MF_SEPARATOR, 0, 0) // separators are not kept in children map
 	return me
 }
 
 // Inserts a new submenu with the given text.
 // Returns newly inserted submenu.
 func (me *Menu) AppendSubmenu(text string) *Menu {
-	newSubmeu := &Menu{
-		text:  text,
-		hMenu: win.CreatePopupMenu(),
+	newSubmenu := &Menu{
+		hMenu:    win.CreatePopupMenu(),
+		children: make(map[string]interface{}),
 	}
-	me.hMenu.AppendMenu(co.MF_STRING|co.MF_POPUP, uintptr(newSubmeu.hMenu),
+	me.hMenu.AppendMenu(co.MF_STRING|co.MF_POPUP, uintptr(newSubmenu.hMenu),
 		uintptr(unsafe.Pointer(win.StrToPtr(text))))
-	me.children = append(me.children, newSubmeu)
-	return newSubmeu
+	me.children[removeAccelAmpersands(text)] = newSubmenu // text kept without ampersands
+	return newSubmenu
 }
 
 // Creates an horizontal main window menu.
@@ -70,6 +67,7 @@ func (me *Menu) CreateMain() *Menu {
 		panic("CreateMain failed: can't create menu twice.")
 	}
 	me.hMenu = win.CreateMenu()
+	me.children = make(map[string]interface{})
 	return me
 }
 
@@ -80,9 +78,12 @@ func (me *Menu) CreatePopup() *Menu {
 		panic("CreatePopup failed: can't create menu twice.")
 	}
 	me.hMenu = win.CreatePopupMenu()
+	me.children = make(map[string]interface{})
 	return me
 }
 
+// Recursively destroys all submenus.
+// Call only if the menu wasn't attached to a window.
 func (me *Menu) Destroy() {
 	if me.hMenu != 0 {
 		me.hMenu.DestroyMenu()
@@ -97,13 +98,13 @@ func (me *Menu) Hmenu() win.HMENU {
 // Don't pass accelerator ampersands in the string.
 // Example: IdOf("File|Open").
 func (me *Menu) IdOf(path string) int32 {
-	entries := strings.Split(path, "|")
+	pathEntries := strings.Split(path, "|")
 	lastSubmenu := me
-	for i := range entries {
-		if i == len(entries)-1 {
-			return lastSubmenu.Item(entries[i]).Id()
+	for i := range pathEntries {
+		if i == len(pathEntries)-1 {
+			return lastSubmenu.Item(pathEntries[i]).Id()
 		}
-		lastSubmenu = lastSubmenu.Submenu(entries[i])
+		lastSubmenu = lastSubmenu.Submenu(pathEntries[i])
 	}
 	return -1 // not found
 }
@@ -111,11 +112,9 @@ func (me *Menu) IdOf(path string) int32 {
 // Returns the first item with the given string, or panics.
 // Don't pass accelerator ampersands in the string.
 func (me *Menu) Item(text string) *menuItem {
-	for _, child := range me.children {
-		if item, ok := child.(*menuItem); ok {
-			if removeAccelAmpersands(item.text) == text {
-				return item
-			}
+	if value, ok := me.children[text]; ok { // key exists?
+		if item, ok := value.(*menuItem); ok { // key holds a menuItem?
+			return item
 		}
 	}
 	panic(fmt.Sprintf("Inexistent menu item text: %s", text))
@@ -124,11 +123,9 @@ func (me *Menu) Item(text string) *menuItem {
 // Returns the first submenu with the given string, or panics.
 // Don't pass accelerator ampersands in the string.
 func (me *Menu) Submenu(text string) *Menu {
-	for _, child := range me.children {
-		if submenu, ok := child.(*Menu); ok {
-			if removeAccelAmpersands(submenu.text) == text {
-				return submenu
-			}
+	if value, ok := me.children[text]; ok { // key exists?
+		if submenu, ok := value.(*Menu); ok { // key holds a Menu?
+			return submenu
 		}
 	}
 	panic(fmt.Sprintf("Inexistent submenu text: %s", text))
@@ -138,14 +135,8 @@ func (me *Menu) Submenu(text string) *Menu {
 
 type menuItem struct {
 	cmdId controlId
-	text  string
 }
 
 func (me *menuItem) Id() int32 {
 	return me.cmdId.Id()
-}
-
-//------------------------------------------------------------------------------
-
-type menuSeparator struct {
 }
