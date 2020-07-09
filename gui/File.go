1bf24a10948ab15a470ca25014c72fdddc132f02
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-07-09 14:47:51 -0300

    High-level File struct.

---
+++ gui/File.go
@@ -0,0 +1,89 @@
+/**
+ * Part of Wingows - Win32 API layer for Go
+ * https://github.com/rodrigocfd/wingows
+ * This library is released under the MIT license.
+ */
+
+package gui
+
+import (
+	"wingows/co"
+	"wingows/win"
+)
+
+// Manages a file resource.
+type File struct {
+	hFile win.HFILE
+}
+
+func (me *File) Close() {
+	if me.hFile != 0 {
+		me.hFile.CloseHandle()
+		me.hFile = 0
+	}
+}
+
+func (me *File) HFile() win.HFILE {
+	return me.hFile
+}
+
+func (me *File) OpenExistingForRead(path string) *File {
+	me.hFile = win.CreateFile(path, co.GENERIC_READ,
+		co.FILE_SHARE_READ, nil, co.FILE_DISPO_OPEN_EXISTING,
+		co.FILE_ATTRIBUTE_NORMAL, co.FILE_FLAG_NONE, co.SECURITY_NONE, 0)
+	return me
+}
+
+func (me *File) OpenExistingForReadWrite(path string) *File {
+	me.hFile = win.CreateFile(path, co.GENERIC_READ|co.GENERIC_WRITE,
+		co.FILE_SHARE_NONE, nil, co.FILE_DISPO_OPEN_EXISTING,
+		co.FILE_ATTRIBUTE_NORMAL, co.FILE_FLAG_NONE, co.SECURITY_NONE, 0)
+	return me
+}
+
+func (me *File) OpenOrCreate(path string) *File {
+	me.hFile = win.CreateFile(path, co.GENERIC_READ|co.GENERIC_WRITE,
+		co.FILE_SHARE_NONE, nil, co.FILE_DISPO_OPEN_ALWAYS,
+		co.FILE_ATTRIBUTE_NORMAL, co.FILE_FLAG_NONE, co.SECURITY_NONE, 0)
+	return me
+}
+
+// Rewinds the file pointer and reads all the raw file contents.
+func (me *File) ReadAll() []uint8 {
+	me.Rewind()
+	sz := me.Size()
+	buf := make([]uint8, sz)
+	me.hFile.ReadFile(buf, sz)
+	me.Rewind()
+	return buf
+}
+
+// Rewinds the file pointer back to the first byte of file.
+func (me *File) Rewind() *File {
+	me.hFile.SetFilePointer(0, co.FILE_SETPTR_BEGIN)
+	return me
+}
+
+// Expands or shrinks the file.
+func (me *File) SetSize(numBytes uint32) *File {
+	me.hFile.SetFilePointer(int32(numBytes), co.FILE_SETPTR_BEGIN)
+	me.hFile.SetEndOfFile()
+	me.Rewind()
+	return me
+}
+
+func (me *File) Size() uint32 {
+	return me.hFile.GetFileSize()
+}
+
+func PathExists(path string) bool {
+	return win.GetFileAttributes(path) != co.FILE_ATTRIBUTE_INVALID
+}
+
+func PathIsFolder(path string) bool {
+	return (win.GetFileAttributes(path) & co.FILE_ATTRIBUTE_DIRECTORY) != 0
+}
+
+func PathIsHidden(path string) bool {
+	return (win.GetFileAttributes(path) & co.FILE_ATTRIBUTE_HIDDEN) != 0
+}
--- win/hfile.go
+++ win/hfile.go
@@ -17,12 +17,17 @@ import (
 type HFILE HANDLE
 
 func (hFile HFILE) CloseHandle() {
-	ret, _, lerr := syscall.Syscall(proc.CloseHandle.Addr(), 1,
-		uintptr(hFile), 0, 0)
+	ret, lerr := hFile.closeHandleNoPanic()
 	if ret == 0 {
 		panic(fmt.Sprintf("CloseHandle failed: %d %s",
 			lerr, lerr.Error()))
 	}
+}
+
+func (hFile HFILE) closeHandleNoPanic() (HFILE, syscall.Errno) {
+	ret, _, lerr := syscall.Syscall(proc.CloseHandle.Addr(), 1,
+		uintptr(hFile), 0, 0)
+	return HFILE(ret), lerr
 }
 
 func CreateDirectory(pathName string, securityAttributes *SECURITY_ATTRIBUTES) {
@@ -64,6 +69,7 @@ func (hFile HFILE) CreateFileMapping(securityAttributes *SECURITY_ATTRIBUTES,
 		0, uintptr(maxSize),
 		uintptr(unsafe.Pointer(StrToPtrBlankIsNil(objectName))))
 	if lerr != 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("CreateFileMapping failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -93,6 +99,7 @@ func (hFile HFILE) GetFileSize() uint32 {
 	ret, _, lerr := syscall.Syscall(proc.GetFileSize.Addr(), 1,
 		uintptr(hFile), 0, 0)
 	if ret == 0xFFFFFFFF && lerr != 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("GetFileSize failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -104,19 +111,22 @@ func (hFile HFILE) GetFileSizeEx() int64 {
 	ret, _, lerr := syscall.Syscall(proc.GetFileSizeEx.Addr(), 2,
 		uintptr(hFile), uintptr(unsafe.Pointer(&buf)), 0)
 	if ret == 0 && lerr != 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("GetFileSizeEx failed: %d %s",
 			lerr, lerr.Error()))
 	}
 	return buf
 }
 
-// Returns the number of bytes read.
+// Returns the number of bytes actually read.
+// Buffer must be previously allocated.
 func (hFile HFILE) ReadFile(buf []uint8, bytesToRead uint32) uint32 {
 	numRead := uint32(0)
 	ret, _, lerr := syscall.Syscall6(proc.ReadFile.Addr(), 5,
 		uintptr(hFile), uintptr(unsafe.Pointer(&buf[0])),
 		uintptr(bytesToRead), uintptr(unsafe.Pointer(&numRead)), 0, 0) // OVERLAPPED not even considered
 	if ret == 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("ReadFile failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -127,6 +137,7 @@ func (hFile HFILE) SetEndOfFile() {
 	ret, _, lerr := syscall.Syscall(proc.SetEndOfFile.Addr(), 1,
 		uintptr(hFile), 0, 0)
 	if ret == 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("SetEndOfFile failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -139,6 +150,7 @@ func (hFile HFILE) SetFilePointer(distanceToMove int32,
 		uintptr(hFile), uintptr(distanceToMove), 0, uintptr(moveMethod),
 		0, 0)
 	if int32(ret) == -1 && lerr != 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("SetFilePointer failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -151,6 +163,7 @@ func (hFile HFILE) SetFilePointerEx(distanceToMove int64,
 		uintptr(hFile), uintptr(distanceToMove), 0, uintptr(moveMethod),
 		0, 0)
 	if ret == 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("SetFilePointerEx failed: %d %s",
 			lerr, lerr.Error()))
 	}
@@ -162,6 +175,7 @@ func (hFile HFILE) WriteFile(buf []uint8) {
 		uintptr(hFile), uintptr(unsafe.Pointer(&buf[0])),
 		uintptr(len(buf)), uintptr(unsafe.Pointer(&written)), 0, 0) // OVERLAPPED not even considered
 	if ret == 0 {
+		hFile.closeHandleNoPanic()
 		panic(fmt.Sprintf("WriteFile failed: %d %s",
 			lerr, lerr.Error()))
 	}
