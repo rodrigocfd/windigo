002355376791ce3c67db143f35e7e4284d404f42
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-06-02 19:13:21 -0300

    GetModuleFileNameEx function.

--- win/psapi_hprocess.go
+++ win/psapi_hprocess.go
@@ -16,16 +16,34 @@ import (
 //
 // [GetModuleBaseName]: https://learn.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulebasenamew
 func (hProcess HPROCESS) GetModuleBaseName(hModule HINSTANCE) (string, error) {
-	var processName [utl.MAX_PATH]uint16
+	var baseName [utl.MAX_PATH]uint16
 	ret, _, err := syscall.SyscallN(_GetModuleBaseNameW.Addr(),
 		uintptr(hProcess),
 		uintptr(hModule),
-		uintptr(unsafe.Pointer(&processName[0])),
-		uintptr(len(processName)))
+		uintptr(unsafe.Pointer(&baseName[0])),
+		uintptr(len(baseName)))
 	if ret == 0 {
 		return "", co.ERROR(err)
 	}
-	return wstr.WstrSliceToStr(processName[:]), nil
+	return wstr.WstrSliceToStr(baseName[:]), nil
 }
 
 var _GetModuleBaseNameW = dll.Psapi.NewProc("GetModuleBaseNameW")
+
+// [GetModuleFileNameEx] function.
+//
+// [GetModuleFileNameEx]: https://learn.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexw
+func (hProcess HPROCESS) GetModuleFileNameEx(hModule HINSTANCE) (string, error) {
+	var fileName [utl.MAX_PATH]uint16
+	ret, _, err := syscall.SyscallN(_GetModuleFileNameExW.Addr(),
+		uintptr(hProcess),
+		uintptr(hModule),
+		uintptr(unsafe.Pointer(&fileName[0])),
+		uintptr(len(fileName)))
+	if ret == 0 {
+		return "", co.ERROR(err)
+	}
+	return wstr.WstrSliceToStr(fileName[:]), nil
+}
+
+var _GetModuleFileNameExW = dll.Psapi.NewProc("GetModuleFileNameExW")
