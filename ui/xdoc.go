d70b9653ff4828355fb8bc8ba8c2d64bdb23a3a0
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2026-02-17 15:36:00 -0300

    Package-level list of UI abstractions.

--- ui/xdoc.go
+++ ui/xdoc.go
@@ -8,6 +8,36 @@
 // resources, which can be manipulated with a WYSIWYG editor like
 // [Visual Studio] or [Resource Hacker].
 //
+// The following windows and controls are implemented:
+//
+//   - [Main] – main application window
+//   - [Modal] – modal window
+//   - [Control] – custom control
+//   - [Button]
+//   - [CheckBox]
+//   - [ComboBox]
+//   - [DateTimePicker]
+//   - [Edit]
+//   - [Header]
+//   - [ListView]
+//   - [MonthCalendar]
+//   - [ProgressBar]
+//   - [RadioGroup] and [RadioButton]
+//   - [Static]
+//   - [StatusBar]
+//   - [SysLink]
+//   - [Tab]
+//   - [Toolbar]
+//   - [Trackbar]
+//   - [TreeView]
+//   - [UpDown]
+//
+// The following interfaces are declared:
+//
+//   - [Window] – any window
+//   - [ChildControl] – a child control window
+//   - [Parent] – a parent window
+//
 // [Visual Studio]: https://visualstudio.microsoft.com/vs
 // [Resource Hacker]: https://www.angusj.com/resourcehacker
 package ui
